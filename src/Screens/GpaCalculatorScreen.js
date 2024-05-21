a78406ce2ae74d086d8f1d1570a101c5ac5704b6
eyualpha <eyuashenafi533@gmail.com>
2024-05-22 00:27:11 +0300

    Created ASTU Grading system for GPA

--- src/Screens/GpaCalculatorScreen.js
+++ src/Screens/GpaCalculatorScreen.js
@@ -1,7 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+const gradeValues = {
+    "A": 4.0,
+    "A+": 4.0,
+    "A-": 3.75,
+    "B+": 3.5,
+    "B": 3.0,
+    "B-": 2.75,
+    "C+": 2.5,
+    "C": 2.0,
+    "C-": 1.75,
+    "D": 1.0,
+    "F": 0.0
+};
+
 const GpaCalculator = () => {
+  const [subjects, setSubjects] = useState([
+    { id: "1", grade: "", credit: "" },
+  ]);
+  const [gpa, setGpa] = useState(null);
+
+  const addSubject = () => {
+    setSubjects([
+      ...subjects,
+      { id: (subjects.length + 1).toString(), grade: "", credit: "" },
+    ]);
+  };
   return (
     <View>
       <Text>GpaCalculator</Text>
