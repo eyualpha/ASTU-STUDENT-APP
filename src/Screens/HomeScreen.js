aea6254c57e5c0b0b6e068b124145bb64b72c350
eyualpha <eyuashenafi533@gmail.com>
2024-05-23 22:26:58 +0300

    styling homeScreen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import React from "react";
+import Color from "../Componets/Color";
+
+
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+
+      <View style = {styles.greetingSection}>
+        <Text style = {styles.greetingTitle}>Hello!</Text>
+        <Text style ={styles.greetingDetail}>Welcome to MY ASTU. The ultimate information guide application for students.</Text>
+      </View>
       <Button
         title="Go to GPA Calculator"
         onPress={() => navigation.navigate("GPA Calculator")}
@@ -33,7 +41,25 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
+  greetingSection:{
+    backgroundColor: Color.primary,
+    height: 100,
+    margin: 15,
+    borderRadius: 10,
+    padding: 8
+  },
+  stikerStyle:{
+    height: 50,
+    width: 50
+  },
+  greetingTitle:{
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  greetingDetail:{
+    fontSize: 15,
+    color: '#fff'
+  }
 });
