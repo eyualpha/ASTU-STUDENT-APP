f63b3ea9015f244a0d0326fb05bb35349596c1f8
eyualpha <eyuashenafi533@gmail.com>
2024-05-25 17:04:06 +0300

    entered campus rule and regulaltion data

---
+++ src/Componets/Data/RuleAndRegulationData.js
@@ -0,0 +1,38 @@
+export const rulesData = [
+  {
+    id: "1",
+    title: "Article 101: Class Attendance",
+    description:
+      "Unless provided otherwise in this Legislation or in a rule to be issued by the Senate, a student is required to attend all lecture, laboratory and practical sessions. In situations where a student, because reasons beyond his/her control fails to attend all sessions minimum of 80% attendace shall be requiered if he/she is to earn credit in a given course. The above however, does not preclude.",
+  },
+  {
+    id: "2",
+    title: "Article 105: Exam Administration",
+    description:
+      "105.1. A list of candidates sitting for an examination shall be prepared in advance of the examination by department to which the students belong and such a list shall be handed over to the invigilators for purposes of checking attendance during the exam. The course coordinator must submit the exam paper a day before the starting time of the exam and collect the exam papers 15 to 20 minutes after the end time of the exam to/from the chair office of the department administering the course with the presence of the course coordinator.The School registrar office shall assign invigilators for exams in the School. For courses offered across Schools, invigilators shall be assigned by the School registrar office from School course instructors in which the students are enrolled. The course instructor shall supervise his/her exam.",
+  },
+  {
+    id: "3",
+    title: "Article 93: Course Wavier",
+    description:
+      "93.1. Where unique reasons exist, the School Managing Council of the respective School may at the request of a student or his/her advisor, permit waiver of a course (s) for the particular student. However, the decision of the School Managing Council shall be endorsed by the senate.",
+  },
+  {
+    id: "4",
+    title: "Article 100: Credit Requirements for Undergraduate Studies",
+    description:
+      "The minimum total credit points for five years bachelor program shall be 150 credit hours while the maximum is 180 credit hours. For dual major and minor the calculation of minimum and maximum credit point shall be governed by the guideline set by the University.The proportion of time (lectures, laboratory works tutorials, home study, etc,) allotted for a particular course/module shall be determined by an academic unit offering that course/module and follow the approval of the DAC and SMC.",
+  },
+  {
+    id: "6",
+    title: "Article 101: Class Attendance",
+    description:
+      "Unless provided otherwise in this Legislation or in a rule to be issued by the Senate, a student is required to attend all lecture, laboratory and practical sessions. In situations where a student, because reasons beyond his/her control fails to attend all sessions minimum of 80% attendace shall be requiered if he/she is to earn credit in a given course. The above however, does not preclude.",
+  },
+  {
+    id: "7",
+    title: "Article 101: Class Attendance",
+    description:
+      "Unless provided otherwise in this Legislation or in a rule to be issued by the Senate, a student is required to attend all lecture, laboratory and practical sessions. In situations where a student, because reasons beyond his/her control fails to attend all sessions minimum of 80% attendace shall be requiered if he/she is to earn credit in a given course. The above however, does not preclude.",
+  },
+];
